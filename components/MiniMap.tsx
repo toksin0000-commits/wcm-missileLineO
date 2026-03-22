2fd3c6e9c5e0db34a1b3f1a565aa6dbb1ada2776
Toksin <toksin@example.com>
2026-03-22 17:41:47 +0100

    smazano

--- components/MiniMap.tsx
+++ components/MiniMap.tsx
@@ -48,7 +48,7 @@ export default function MiniMap({ area }: MiniMapProps) {
       </MapContainer>
 
       {/* Digitální šum */}
-      <div className="absolute inset-0 pointer-events-none bg-cyan-500/5 opacity-20 bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] bg-[length:100%_2px,3px_100%]"></div>
+      <div className="absolute inset-0 pointer-events-none bg-cyan-500/5 opacity-20 bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] bg-size-[100%_2px,3px_100%]"></div>
     </div>
   );
 }
